0238fd8a01ba9cdfccd4fed89d7e1156726327a5
Michał Kulik <kulikmichal@proton.me>
2024-11-26 23:56:18 +0100

    finished sidebar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return (
+        <div className="flex items-center justify-between p-4">
+            <div className="hidden md:flex"></div>
+            <div className=""></div>
+        </div>
+    );
+}
