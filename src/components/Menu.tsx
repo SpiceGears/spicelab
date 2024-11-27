9db74cbd4d9688c57eb5d570cdc1c9b455bf2819
Michal Kulik <kulikmichal@proton.me>
2024-11-27 10:44:44 +0100

    Navbar almost finished

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,52 +1,74 @@
-import Image from "next/image";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFolderOpen,
+  faUser,
+  faGear,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Menu() {
-    const menuItems = [
+  const menuItems = [
+    {
+      title: "MENU",
+      items: [
         {
-            title: "MENU",
-            items: [
-            {
-                label: "Home",
-                href: "/dashboard/home",
-            },
-            {
-                label: "Projects",
-                href: "/dashboard/projects",
-            },
-            {
-                label: "Profile",
-                href: "/dashboard/profile",
-            },
-            ],
+          label: "Strona Główna",
+          href: "/dashboard/home",
+          icon: faHouse,
         },
         {
-            title: "OTHER",
-            items: [
-            {
-                label: "Settings",
-                href: "/dashboard/settings",
-            },
-            {
-                label: "Logout",
-                href: "/dashboard/logout",
-            },
-            ],
-        }
-        ]
-    return (
-        <div className="">
-            {menuItems.map((i) => (
-                <div className="flex flex-col gap-2" key={i.title}>
-                    <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
-                    {i.items.map((item) => (
-                        <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-                            <Image src="/icon.ico" alt="Logo" width={20} height={20} />
-                            <span className="hidden lg:block">{item.label}</span>
-                        </Link>
-                    ))}
-                </div>
-            ))}
+          label: "Projekty",
+          href: "/dashboard/projects",
+          icon: faFolderOpen,
+        },
+        {
+          label: "Profil",
+          href: "/dashboard/profile",
+          icon: faUser,
+        },
+      ],
+    },
+    {
+      title: "OTHER",
+      items: [
+        {
+          label: "Ustawienia",
+          href: "/dashboard/settings",
+          icon: faGear,
+        },
+        {
+          label: "Wyloguj",
+          href: "/dashboard/logout",
+          icon: faRightFromBracket,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-2 text-gray-500 py-1"
+            >
+              <FontAwesomeIcon
+                icon={item.icon}
+                className="w-6 h-6 text-black"
+              />
+              <span className="hidden lg:block text-sm">{item.label}</span>
+            </Link>
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,26 @@
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faMessage, faBell } from "@fortawesome/free-solid-svg-icons";
+
 export default function Navbar(){
     return (
         <div className="flex items-center justify-between p-4">
-            <div className="hidden md:flex"></div>
-            <div className=""></div>
+        <div className="hidden md:flex">
+            <FontAwesomeIcon icon={faSearch} className="w-4 h-4" />
+            <input type="text" placeholder="Szukaj..."/>
+        </div>
+        <div className="flex items-center gap-6">
+            <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+                <FontAwesomeIcon icon={faMessage} className="w-4 h-4" />
+            </div>
+            <div className="bg-white rounded-full w-h h-7 flex items-center justify-center cursor-pointer">
+                <FontAwesomeIcon icon={faBell} className="w-4 h-4" />
+            </div>
+            <div className="flex flex-col">
+                <span className="text-xs leading-3 font-medium">Jan Kowalski</span>
+            </div>
+            <Image src="/icons/avatar.png" alt="Avatar" width={40} height={40} className="rounded-full" />
+        </div>
         </div>
     );
 }
