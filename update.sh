1846886b7a34e5b38dba9d8d7a16343af30ae4e6
xszym <xszym69@gmail.com>
2025-01-14 16:38:23 +0100

    Add timestamp

--- update.sh
+++ update.sh
@@ -5,9 +5,11 @@ cd /home/ubuntu/spicelab
 # Perform git pull and capture output
 GIT_OUTPUT=$(git pull)
 
+CURRENT_DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
+
 # Check if there are updates (if output does not contain 'Already up to date.')
 if [[ "$GIT_OUTPUT" != *"Already up to date."* ]]; then
-    echo "Changes pulled from repository. Rebuilding and restarting Docker containers..."
+    echo "[$CURRENT_DATETIME] Changes pulled from repository. Rebuilding and restarting Docker containers..." >> update.log
 
     # Build Docker containers
     docker compose -f docker-compose.prod.yml build
@@ -18,5 +20,5 @@ if [[ "$GIT_OUTPUT" != *"Already up to date."* ]]; then
     # Start containers
     docker compose -f docker-compose.prod.yml up -d
 else
-    echo "No updates were pulled. Docker containers are not restarted."
+    echo "[$CURRENT_DATETIME] No updates were pulled. Docker containers are not restarted." >> update.log
 fi
